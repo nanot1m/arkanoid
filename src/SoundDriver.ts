0832b6a20352a37d3b8afea80d972e6b9144d58d
Хазамов Теймураз Артурович <t1mmaas@skbkontur.ru>
2016-11-14 12:50:04 +0500

    included sound driver

---
+++ src/SoundDriver.ts
@@ -0,0 +1,13 @@
+export class SoundDriver {
+  private static audio: AudioContext =
+    new (window['AudioContext'] || window['webkitAudioContext'])()
+
+  static play(key: number) {
+    let oscillator = SoundDriver.audio.createOscillator();
+    oscillator.connect(SoundDriver.audio.destination);
+    oscillator.type = 'square';
+    oscillator.frequency.value = Math.pow(2, (key - 49) / 12) * 440;
+    oscillator.start();
+    oscillator.stop(SoundDriver.audio.currentTime + 0.100);
+  }
+}
